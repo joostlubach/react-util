4208ff9938b4ca89e2c18510fefbda4586e4bb0c
daanaerts <daanaerts@gmail.com>
2025-04-17 12:28:10 +0200

    use ref fix

--- src/hooks/change-monitor.ts
+++ src/hooks/change-monitor.ts
@@ -4,7 +4,7 @@ import React from 'react'
 export function useChangeMonitor<T>(arg: T, callback: (prev: T) => any, options: ChangeMonitorOptions<T> = {}): boolean {
   const {equals = isEqual} = options
 
-  const prevRef = React.useRef<T>()
+  const prevRef = React.useRef<T>(undefined as any)
   const changed = prevRef.current !== undefined && !equals(arg, prevRef.current)
 
   if (changed) {
