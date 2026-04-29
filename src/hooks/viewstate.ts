3f13fdb904e4fef86388a41c7bf8fd71326fff6f
Joost Lubach <joostlubach@gmail.com>
2026-04-29 10:57:58 +0200

    Fix in the way the viewstate params work.

--- src/hooks/viewstate.ts
+++ src/hooks/viewstate.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 
 const STORAGE: Storage | null = 'localStorage' in globalThis ? globalThis.localStorage : null
 
@@ -6,10 +6,12 @@ export function useViewState<T>(key: undefined, initialValue?: T): ViewStateHook
 export function useViewState<T>(key: string, initialValue: T): ViewStateHook<T>
 export function useViewState<T>(key: string | undefined, initialValue?: T): ViewStateHook<T | undefined>
 export function useViewState<T>(key: string | undefined, initialValue: T): ViewStateHook<T> {
-  const [value, setValueState] = useState<T | undefined>(() => {
-    if (key === undefined) { return initialValue }
+  const [lastStoredAt, setLastStoredAt] = useState(Date.now())
 
-    const serialized = STORAGE?.getItem(key)
+  const value = useMemo<T | undefined>(() => {
+    const _ = lastStoredAt
+
+    const serialized = key !== undefined ? STORAGE?.getItem(key) : undefined
     if (serialized == null) { return initialValue }
 
     try {
@@ -17,9 +19,9 @@ export function useViewState<T>(key: string | undefined, initialValue: T): ViewS
     } catch {
       return initialValue
     }
-  })
+  }, [lastStoredAt, key, initialValue])
 
-  // When setting the value, update local storage and the local cache.
+  // When setting the value, update local storage and break cache by setting last stored at to now.
   const setValue = useCallback((value: T) => {
     if (key === undefined) { return }
 
@@ -28,14 +30,14 @@ export function useViewState<T>(key: string | undefined, initialValue: T): ViewS
     } else {
       STORAGE?.setItem(key, JSON.stringify(value))
     }
-    setValueState(value)
+    setLastStoredAt(Date.now())
   }, [key])
 
-  // When setting the value, update local storage and the local cache.
+  // When deleting the value, update local storage and break cache by setting last stored at to now.
   const deleteValue = useCallback(() => {
     if (key === undefined) { return }
     STORAGE?.removeItem(key)
-    setValueState(undefined)
+    setLastStoredAt(Date.now())
   }, [key])
 
   return [value as T, setValue, deleteValue]
